b784ea81f0a23c2c200a1163aa70d9d939ec156c
Jace Laquerre <jace.laquerre@uvm.edu>
2020-11-03 15:05:05 -0500

    firebase stuff

---
+++ FilmNite/FilmNite/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  FilmNite
+//
+//  Created by Jace on 10/29/20.
+//
+
+import Foundation
+import UIKit
+
+class Movie {
+    var title: String = ""
+    
+    init() {
+        
+    }
+}
