c5e4ba148d3c0ea1880ff11c75b87c3c87e05496
jbrown63 <jack.brown@uvm.edu>
2020-11-17 10:42:21 -0500

    Create session view, # of movies label, year range, started genre

--- FilmNite/FilmNite/ViewController.swift
+++ FilmNite/FilmNite/ViewController.swift
@@ -7,8 +7,14 @@
 //test; Jack; V2
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var startDate: UIPickerView!
+    @IBOutlet weak var endDate: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,8 +23,32 @@ class ViewController: UIViewController {
             //tap.cancelsTouchesInView = false
 
             view.addGestureRecognizer(tap)
+        
+        
+        self.startDate?.delegate = self
+        self.startDate?.dataSource = self
+        
+        self.endDate?.delegate = self
+        self.endDate?.dataSource = self
+        
+        pickerData = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]
     
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    
+    
     @objc func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
