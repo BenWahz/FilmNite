3a3b6c503b813be74aa0ed3d12606b76a5d96915
Jace Laquerre <jace.laquerre@uvm.edu>
2020-11-19 12:18:23 -0500

    work on backend

--- FilmNite/FilmNite/Database.swift
+++ FilmNite/FilmNite/Database.swift
@@ -10,23 +10,6 @@ import Firebase
 
 var ref: DatabaseReference!
 
-func createSession(sessionID: String, users: [User], requestURL: String) {
-    ref = Database.database().reference()
-    var ct = 0
-    let sessionMovies = getMovies(requestURL: requestURL)
-    //for movie in sessionMovies {
-    ref.child(sessionID).child("Session Movies").setValue(sessionMovies)
-        //ct += 1
-    //}
-    ct = 0
-    for user in users {
-        for movie in user.movies {
-            ref.child(sessionID).child(user.username).setValue([String(ct): movie.netflixid])
-            ct += 1
-        }
-    }
-}
-
 func addUserToSession(sessionID: String, user: User) {
     ref = Database.database().reference()
     ref.child(sessionID).child(user.username).setValue(["initial": 0])
@@ -41,9 +24,10 @@ func updateUser(sessionID: String, user: User) {
     }
 }
 
-func getMovies(requestURL: String) -> [Dictionary<String, String>] {
+func createSession(sessionID: String, user: User, requestURL: String) {
+    ref = Database.database().reference()
     /// NETFLIX API //////////////////////////////////////////////////////////////////////////////
-    var movieList = [Dictionary<String, String>]()
+    //var movieList = [Dictionary<String, String>]()
     let headers = [
         "x-rapidapi-key": "305c976e8bmshb63b13d0d6334c9p1aaa99jsn0c2f6eeefa67",
         "x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com"
@@ -71,35 +55,81 @@ func getMovies(requestURL: String) -> [Dictionary<String, String>] {
                 
                 if let dictionary = json as? [String: Any] {
                     if let array = dictionary["ITEMS"] as? [Dictionary<String, String>] {
-                        movieList = array
-//                        for object in array {
-                            //var movie = Movie()
-//                            for (key, value) in object {
-//                                if key == "netflixid" {
-//                                    movie.netflixid = Int(value) ?? -1
-//                                } else if key == "title" {
-//                                    movie.title = value
-//                                } else if key == "image" {
-//                                    movie.image = value
-//                                } else if key == "synopsis" {
-//                                    movie.synopsis = value
-//                                } else if key == "released" {
-//                                    movie.released = Int(value) ?? -1
-//                                } else if key == "rating" {
-//                                    movie.rating = value
-//                                }
-                         //   }
-//                            movieList.append(movie)
-                     //   }
-                    return
+                        ref.child(sessionID).child("sessionMovies").setValue(array)
+                        return
                     } else {
                         print("Error Accessing Items")
                     }
                 }
         }
     })
-    print("-----------------")
-    print(movieList)
-    return movieList
+    ref.child(sessionID).child(user.username).setValue(["initial": 0])
+    ref.child(sessionID).child("commonMovies").setValue(["initial": 0])
+    user.movies = getSessionMovies(sessionID: sessionID)
     dataTask.resume()
+}
+
+func getSessionMovies(sessionID: String) -> [Movie] {
+    //ref = Database.database().reference()
+    var movieList = [Movie]()
+    var movie = Movie()
+    
+    let ref = Database.database().reference(withPath: sessionID)
+    let movieRef = ref.child("sessionMovies")
+    ref.observe(.value, with: { snapshot in
+        // This is the snapshot of the data at the moment in the Firebase database
+        // To get value from the snapshot, we user snapshot.value
+        //in my case the answer is of type array so I can cast it like this, should also work with NSDictionary or NSNumber
+        makeMovies(snapshot: snapshot)
+//        let array = snapshot.value as? NSArray
+//        for movie in array! {
+//            for (key, value) in movie as! Dictionary<String, String> {
+//                if key == "netflixid" {
+//                    movie.netflixid = Int(value) ?? -1
+//                } else if key == "title" {
+//                    movie.title = value
+//                } else if key == "image" {
+//                    movie.image = value
+//                } else if key == "synopsis" {
+//                    movie.synopsis = value
+//                } else if key == "released" {
+//                    movie.released = Int(value) ?? -1
+//                } else if key == "rating" {
+//                    movie.rating = value
+//                }
+//            }
+//        }
+    })
+    
+    movieList.append(movie)
+    return movieList
+}
+
+func makeMovies(snapshot: DataSnapshot) -> [Movie] {
+        var movies = [Movie]()
+        if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+            for snap in snapshots {
+                if let postDictionary = snap.value as? Dictionary<String, String> {
+                    let movie = Movie()
+                    for (key, value) in postDictionary {
+                        if key == "netflixid" {
+                            movie.netflixid = Int(value) ?? -1
+                        } else if key == "title" {
+                            movie.title = value
+                        } else if key == "image" {
+                            movie.image = value
+                        } else if key == "synopsis" {
+                            movie.synopsis = value
+                        } else if key == "released" {
+                            movie.released = Int(value) ?? -1
+                        } else if key == "rating" {
+                            movie.rating = value
+                        }
+                    }
+                    movies.append(movie)
+                }
+            }
+        }
+    print(movies)
+    return movies
 }
