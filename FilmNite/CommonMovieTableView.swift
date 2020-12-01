ec61be34a90bd7cff5fcf69ea514b5445f8a7234
Sarah O'Brien <snobrien@uvm.edu>
2020-12-01 15:08:08 -0500

    card view backgroundchanges- making text readable

---
+++ FilmNite/CommonMovieTableView.swift
@@ -0,0 +1,18 @@
+//
+//  CommonMovieTableView.swift
+//  FilmNite
+//
+//  Created by Sarah O'Brien on 12/1/20.
+//
+
+import Foundation
+import UIKit
+
+class CommonMovieTableView: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
